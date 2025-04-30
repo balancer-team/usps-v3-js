9e8729781c1763781537c36a933cd462cd949996
alanhett <ahettinger@gmail.com>
2025-04-30 19:09:27 -0400

    Updated tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -10,34 +10,23 @@ const clientSecret = process.env.USPS_CLIENT_SECRET
 
 const usps = new USPS({ clientId, clientSecret })
 
-test('Test test', () => {
-  console.log(usps)
-})
-
 test('Address lookup', async () => {
-  const address = await usps.getAddress({
+  const data = await usps.getAddress({
     streetAddress: '302 Riverside Drive',
     city: 'Melbourne Beach',
     state: 'FL',
   })
 
-  console.log(address)
-
-  // assert.strictEqual(address.firm, 'USPS')
-  // assert.strictEqual(address.address.Address2, '123 MAIN ST')
-  // assert.strictEqual(address.address.City, 'SPRINGFIELD')
-  // assert.strictEqual(address.address.State, 'IL')
-  // assert.strictEqual(address.address.Zip5, '62701')
+  assert.strictEqual(data.address.ZIPCode, '32951')
+  assert.strictEqual(data.address.ZIPPlus4, '2142')
 })
 
 test('City/State lookup', async () => {
-  const cityState = await usps.getCityState({
+  const data = await usps.getCityState({
     ZIPCode: '90210',
   })
 
-  console.log(cityState)
-
-  // assert.strictEqual(cityState.city, 'BEVERLY HILLS')
-  // assert.strictEqual(cityState.state, 'CA')
-  // assert.strictEqual(cityState.ZIPCode, '90210')
+  assert.strictEqual(data.city, 'BEVERLY HILLS')
+  assert.strictEqual(data.state, 'CA')
+  assert.strictEqual(data.ZIPCode, '90210')
 })
